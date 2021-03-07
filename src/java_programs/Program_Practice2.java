df1837aef6bbce1b756cda1c526471d7c5a60bd5
Ashutosh <ashutosh.singh12@gmail.com>
2021-03-07 19:23:04 +0530

    Final commit for Program_Practice1

--- src/java_programs/Program_Practice2.java
+++ src/java_programs/Program_Practice2.java
@@ -3,6 +3,6 @@ package java_programs;
 public class Program_Practice2 {
 
     public void methodFirst() {
-        System.out.print("My Method");
+        System.out.print("My Method 1 ....");
     }
 }
